70a9509c218f23223e588172fdec51e71899f7cf
flochtililoch <flochtililoch@gmail.com>
2016-12-14 09:59:20 -0800

    Add body parser and allow using body to fingerprint requests. Remove CLI arguments, and only accept a config path argument. Allow specifying fingerprint params via config.

--- middleware/fingerprint.js
+++ middleware/fingerprint.js
@@ -1,33 +1,22 @@
 'use strict';
 
-const hash = require('object-hash');
+const hash = require('object-hash'),
+  {prettify} = require('../lib/json');
 
-const filters = {
-  url: null,
-  method: null,
-  params: null,
-  query: null,
-  body: null,
-  headers: ['user-agent', 'accept-language']
-};
+module.exports = filters => (req, res, next) => {
+  const process = filter => {
+    switch (filter.constructor) {
+      case Object:
+      return Object
+        .keys(filter)
+        .map(key => filter[key].map(subfilter => req[key][subfilter]));
+      case String:
+        return {[filter]: req[filter]};
+    }
+  };
 
-const process = (subfilters, data) => {
-  if (subfilters && subfilters.length > 0) {
-    return Object.keys(data)
-      .filter(key => subfilters.includes(key))
-      .reduce((obj, key) => {
-        obj[key] = data[key];
-        return obj;
-      }, {});
-  } else {
-    return data;
-  }
-};
+  req.fingerprint = prettify(filters.map(process));
+  req.id = hash(req.fingerprint);
 
-module.exports = (req, res, next) => {
-  const dictionary = Object.keys(filters).map(filter => {
-    return process(filters[filter], req[filter]);
-  });
-  req.fingerprint = hash(dictionary);
   next();
 };
--- middleware/record.js
+++ middleware/record.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const {createProxyServer} = require('http-proxy'),
-      resolve = require('../lib/resolve'),
+      {resolveRequestPath} = require('../lib/resolve'),
       write = require('../lib/write');
 
-module.exports = ({target, dir}) => {
+module.exports = ({target, directory}) => {
   const proxy = createProxyServer({target});
+
   proxy.on('proxyRes', (proxyRes, req) => {
     let response = '';
     proxyRes
       .on('data', chunk => response += chunk)
       .on('end', () => {
-        const filename = resolve(req, dir),
+        const filename = resolveRequestPath(req, directory),
               {headers} = proxyRes;
         write(filename, headers, response);
       });
