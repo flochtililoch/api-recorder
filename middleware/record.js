4db6c858496bae6fc2969e39d711cf28ed522c10
flochtililoch <flochtililoch@gmail.com>
2016-12-19 08:51:59 -0800

    Restream POST requests.

--- middleware/record.js
+++ middleware/record.js
@@ -7,6 +7,17 @@ const {createProxyServer} = require('http-proxy'),
 module.exports = ({target, directory}) => {
   const proxy = createProxyServer({target});
 
+  // https://github.com/nodejitsu/node-http-proxy/issues/180
+  // https://github.com/nodejitsu/node-http-proxy/blob/d0e000e1f91a969f1711eb3be7d1acb16c4538df/examples/middleware/bodyDecoder-middleware.js#L37-L47
+  proxy.on('proxyReq', (proxyReq, req) => {
+    if(req.body) {
+      const body = JSON.stringify(req.body);
+      proxyReq.setHeader('Content-Type','application/json');
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(body));
+      proxyReq.write(body);
+    }
+  });
+
   proxy.on('proxyRes', (proxyRes, req) => {
     let response = '';
     proxyRes
