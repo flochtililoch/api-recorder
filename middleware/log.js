acb1fd09bd0bf14e8978d6996f04a585c018b0d2
flochtililoch <flochtililoch@gmail.com>
2016-12-30 14:32:13 -0800

    Disable log by config.

--- middleware/log.js
+++ middleware/log.js
@@ -2,8 +2,14 @@
 
 require('colors');
 
-module.exports = (req, res, next) => {
-  const out = `[${req.id.magenta}] ${req.method.cyan} ${req.path.yellow}`;
-  console.log(out);
-  next();
+module.exports = config => {
+  return (req, res, next) => {
+    // For now, just allow disable logging from config.
+    // Might add more options in the future.
+    if (config.log !== false) {
+      const out = `[${req.id.magenta}] ${req.method.cyan} ${req.path.yellow}`;
+      console.log(out);
+    }
+    next();
+  };
 };
